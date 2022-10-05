04317d1458299b7185c9e6cd169a185567572919
Beatriz Francisconi <beatrizfrancisconi@souunisuam.com.br>
2022-10-05 12:42:30 -0300

    Tela favoritos 50% pronta

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header';
+import Favoritos from './pages/Favorito';
+
 import Erro from './pages/Erro';
 
 function RoutesApp() {
@@ -11,7 +13,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filme/:id" element={ <Filme/> } />
-               
+                <Route path="/favoritos" element={ <Favoritos /> } />
                
                
                
